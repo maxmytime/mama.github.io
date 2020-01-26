625a7d1ae4737351797b2e623d447888d4385ef5
maxmytime <42465581+maxmytime@users.noreply.github.com>
2020-01-26 21:43:11 +0700

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,5 +1,29 @@
 $(function() {
 
-	// Custom JS
+	$('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
 
 });
+
+var purchasesToggle = document.querySelectorAll('.purchases-toggle');
+console.log(purchasesToggle[0].classList);
+
+function clicPurchasesToggle() {
+    purchasesToggle[0].classList.toggle('active');
+}
+
+purchasesToggle[0].addEventListener('click', clicPurchasesToggle);
