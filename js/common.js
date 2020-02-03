bb5e90ac1b32b7d0b42cd60fe4e9710c84b48bcc
maxmytime <42465581+maxmytime@users.noreply.github.com>
2020-02-04 00:20:14 +0700

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,9 +1,55 @@
 $(function() {
 
-	$('.owl-carousel').owlCarousel({
+	$('.slider-1').owlCarousel({
         loop:true,
         margin:10,
         nav:true,
+        dots: false,
+        responsive:{
+            0:{
+                items:3
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+
+});
+
+$(function() {
+
+	$('.slider-2').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots: true,
+        pagination : true,
+        responsive:{
+            0:{
+                items:3
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
+        }
+    })
+
+});
+
+$(function() {
+
+	$('.slider-3').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:true,
+        dots: false,
         responsive:{
             0:{
                 items:3
@@ -28,10 +74,11 @@ $(function() {
 
 // purchasesToggle[0].addEventListener('click', clicPurchasesToggle);
 
-var purchasesToggle = document.querySelectorAll('.purchases');
+var purchases = document.querySelectorAll('.purchases');
+var purchasesToggle = document.querySelectorAll('.purchases-toggle');
 
 function clicPurchasesToggle() {
-    purchasesToggle[0].classList.toggle('active');
+    purchases[0].classList.toggle('active');
 }
 
 purchasesToggle[0].addEventListener('click', clicPurchasesToggle);
@@ -45,3 +92,16 @@ jQuery(function($){
 		}
 	});
 });
+
+$(document).on('click', function(e) {
+    if ($(window).width() <= '991') {
+        if (!$(e.target).closest(".menu-toggle").length) {
+            $('nav').hide();
+          }
+        e.stopPropagation();
+    }
+
+  });
+
+
+
